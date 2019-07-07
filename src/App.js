d68f4a389deedc7539d32a8212a1aeec1a3bf765
Josemaria <josemarianriagu25@gmail.com>
2019-07-08 01:12:13 +0800

    implement charge indicator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+
+  componentDidMount() {
+    if (this.state.count < 101) {
+      setInterval(() => {
+        this.setState({ count: this.state.count + 1 })
+        if (this.state.count === 101) {
+          this.setState({ count: 0 })
+        }
+      }, 66000);
+    }
+  }
+
+  render() {
+    return (
+      <div className="app" >
+        <div className="counter">{this.state.count}<span>%</span></div>
+      </div>
+    );
+  }
 }
 
 export default App;
